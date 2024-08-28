d22637e1ed0c72692753ee76b9b0059d6ea9143a
windy <707298413@qq.com>
2024-08-29 00:36:09 +0800

    rm explicit ref type

--- src/parser/Parser+Util.cpp
+++ src/parser/Parser+Util.cpp
@@ -10,54 +10,50 @@ std::shared_ptr<wind::Type> Parser::tryParseMarkedTy(std::shared_ptr<Env> env, s
 }
 
 std::shared_ptr<wind::Type> Parser::parseTy(std::shared_ptr<Env> env) {
-    if (tryEat("'")) {
-        return std::make_shared<wind::ReferenceType>(parseTy(env));
-    } else {
-        std::string name = parseIdent();
-        std::vector<std::shared_ptr<wind::Type>> parameters;
-        if (curTk->is('<')) {
-            eat("<");
-            parameters = parseTyList(env, ',');
-            eat(">");
-        }
-        if (parameters.empty()) {
-            if (name == "void") {
-                return wind::Type::VOID;
-            } else if (name == "ptr") {
-                return wind::Type::PTR;
-            } else if (name == "bool") {
-                return wind::Type::BOOL;
-            } else if (name == "i8") {
-                return wind::Type::I8;
-            } else if (name == "i16") {
-                return wind::Type::I16;
-            } else if (name == "i32") {
-                return wind::Type::I32;
-            } else if (name == "i64") {
-                return wind::Type::I64;
-            } else if (name == "u8") {
-                return wind::Type::U8;
-            } else if (name == "u16") {
-                return wind::Type::U16;
-            } else if (name == "u32") {
-                return wind::Type::U32;
-            } else if (name == "u64") {
-                return wind::Type::U64;
-            } else if (name == "f32") {
-                return wind::Type::F32;
-            } else if (name == "f64") {
-                return wind::Type::F64;
-            } else {
-                if (auto s = env->lookup(name)) {
-                    if (s->ty == SymbolType::BOX_TYPE) { // generic type
-                        return s->boxtype;
-                    }
-                }
-                return std::make_shared<wind::NamedType>(name);
-            }
+    std::string name = parseIdent();
+    std::vector<std::shared_ptr<wind::Type>> parameters;
+    if (curTk->is('<')) {
+        eat("<");
+        parameters = parseTyList(env, ',');
+        eat(">");
+    }
+    if (parameters.empty()) {
+        if (name == "void") {
+            return wind::Type::VOID;
+        } else if (name == "ptr") {
+            return wind::Type::PTR;
+        } else if (name == "bool") {
+            return wind::Type::BOOL;
+        } else if (name == "i8") {
+            return wind::Type::I8;
+        } else if (name == "i16") {
+            return wind::Type::I16;
+        } else if (name == "i32") {
+            return wind::Type::I32;
+        } else if (name == "i64") {
+            return wind::Type::I64;
+        } else if (name == "u8") {
+            return wind::Type::U8;
+        } else if (name == "u16") {
+            return wind::Type::U16;
+        } else if (name == "u32") {
+            return wind::Type::U32;
+        } else if (name == "u64") {
+            return wind::Type::U64;
+        } else if (name == "f32") {
+            return wind::Type::F32;
+        } else if (name == "f64") {
+            return wind::Type::F64;
         } else {
-            return std::make_shared<wind::NamedType>(name, parameters);
+            if (auto s = env->lookup(name)) {
+                if (s->ty == SymbolType::BOX_TYPE) { // generic type
+                    return s->boxtype;
+                }
+            }
+            return std::make_shared<wind::NamedType>(name);
         }
+    } else {
+        return std::make_shared<wind::NamedType>(name, parameters);
     }
 }
 
